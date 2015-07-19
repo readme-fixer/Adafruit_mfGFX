3cc61571633186f25e5efe98a57aa2bfb0de9c8b
Duane Johnson <duane.johnson@gmail.com>
2015-07-19 09:46:26 -0600

    Remove trailing backslashes from character comments

--- Adafruit_mfGFX.cpp
+++ Adafruit_mfGFX.cpp
@@ -1,2 +1,7 @@
 #include "Adafruit_mfGFX.h"
 
+// This file is intentionally left blank, so that the header class can be
+// statically configured from the Arduino project using #define statements.
+// This is done in order to provide a wide variety of font options, while
+// loading only the ones that will be used by a project (preserving flash
+// space).
